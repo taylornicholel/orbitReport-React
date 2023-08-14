f6e6c221669a8495f4c8e0a9de0c6316dd3c1b7f
taylornicholel <139183007+taylornicholel@users.noreply.github.com>
2023-08-14 08:31:03 -0500

    finished banner

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+
+
+function Banner () {
+    <head> // adding head because I needed a parent
+        <header>Orbit Report</header>
+        <p>Click on the buttons to see the satellites in that orbit type </p>
+    </head>
+};
+
+export default Banner; 
