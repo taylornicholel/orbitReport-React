6c0f8a08db2356753b03ea56bfd2115c1d397cdc
taylornicholel <139183007+taylornicholel@users.noreply.github.com>
2023-08-14 08:58:57 -0500

    task 3 update app file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import Banner from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import satData from "./components/satData";
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import './App.css';
 
+// What's the difference between from/as : "Import useState from "react". & Import satData.js as satData." ???? 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  // code continues ...
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+ };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons
+        filterByType={filterByType} // why is this variable not bluet like the others 8/14 8:57
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
+
+
+
 
 export default App;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,10 @@
 
 
 function Banner () {
-    <head> // adding head because I needed a parent
+    <head> 
         <header>Orbit Report</header>
         <p>Click on the buttons to see the satellites in that orbit type </p>
     </head>
-};
+}; // adding head because I needed a parent
 
 export default Banner; 
