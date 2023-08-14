727d09ba446384b1d757178c49b773a5537555a7
taylornicholel <139183007+taylornicholel@users.noreply.github.com>
2023-08-14 12:31:59 -0500

    post office hours TA Ariel

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,28 @@
-const Buttons = () => {
-  return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+import satData from "./satData";
+// Pass the props created from the App() function: filterByType, setSat, and displaySats.
+
+/* INCORRECT
+import  {
+  filterByType, 
+  setSat, 
+  displaySats
+} from './App' */ 
+
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
+  {displaySats.map((sat, id) => {
+    return (
+      <div>
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+        //code continues
+        <button>All Orbits</button>
       </div>
-  );
+    );
+  })};
 };
+
+
 
 export default Buttons;
